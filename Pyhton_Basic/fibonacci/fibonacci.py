c49816b39dc6dab610935ab4b162293261ad685c
mdyousufhossain <yousafhossain3@gmail.com>
2024-01-09 23:40:21 +0600

    fibonacci

---
+++ Pyhton_Basic/fibonacci/fibonacci.py
@@ -0,0 +1,27 @@
+a,b = 0,1 
+
+while a < 100: 
+     print(a)
+     a,b = b , a + b
+     
+     
+     
+# a = 0 , b = 1 , until a reach 100 we will sum value  
+# so first value of a = 0 but b is 1 
+# so lets explain
+
+# ---------------------- 
+#(previus value of 'a' sum new value of'b' )
+# a = 0 , b = 1  
+# a = 1  b = 1 ,b= 0 + 1
+# a = 1  b = 1 ,b= 1 + 1  
+# a = 2  b = 2 ,b= 1 + 2
+# a = 3  b = 3 ,b= 2 + 3
+# a = 5  b = 5 ,b= 3 + 5
+# a = 8  b = 8 ,b= 5 + 8
+# a = 13 b = 13,b= 8 + 13
+# a = 21 b = 21,b= 13 + 21
+# a = 34  
+ 
+    
+    
