ba5df2320b80f534b62bc1ef293f754b94f57217
mdyousufhossain <yousafhossain3@gmail.com>
2024-01-09 20:04:11 +0600

    arithmetic

---
+++ Pyhton_Basic/arithmetic.py
@@ -0,0 +1,14 @@
+# basic operators - + * / % //
+sum = 2+2 
+
+multiply = sum * (2*2) #using bracket for grouping 
+
+division = (multiply +3)/sum #normal division that return a float 
+
+omitedfloorDivision = multiply//sum #divison that discards the fractional part 16.100 --> 16
+
+
+usingPowerInPy = 5**2# 5squire --> 25
+
+
+
