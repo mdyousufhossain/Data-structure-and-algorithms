93bc0927d76206019f241cb6a4d1a12413b55547
mdyousufhossain <yousafhossain3@gmail.com>
2024-01-11 20:10:20 +0600

    for loop funtion added

--- Pyhton_Basic/for/for.py
+++ Pyhton_Basic/for/for.py
@@ -2,5 +2,10 @@ arr = [1,2,3,4,5,'...']
 
 for i in range(len(arr)):
     print(i, arr[i])
-    
+
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+for user, status in users.copy().items():
+    print(user,status)
+    if status == 'inactive':
+        del users[user]
  
