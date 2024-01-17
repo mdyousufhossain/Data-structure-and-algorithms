21e72454e1fc4a0de13448eb1e966c166b19609b
mdyousufhossain <yousafhossain3@gmail.com>
2024-01-17 21:16:50 +0600

    doing lot of loops

---
+++ Pyhton_Basic/data structure/dic.py
@@ -0,0 +1,6 @@
+dic1 = {'val' : 1 , 'val2' : 2 , 'val3' : 3 }
+
+
+for key in dic1: 
+    print(key,dic1[key])
+
--- Pyhton_Basic/loops/for.py
+++ Pyhton_Basic/loops/for.py
@@ -1,11 +1,49 @@
-arr = [1,2,3,4,5,'...']
+arr = {}
 
-for i in range(len(arr)):
-    print(i, arr[i])
 
-users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
-for user, status in users.copy().items():
-    print(user,status)
-    if status == 'inactive':
-        del users[user]
+
+# for i in range(len(arr)):
+#     print(i, arr[i])
+
+# user = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+
+# for user, status in users.copy().items():
+#     #print(user,status)
+#     if status == 'inactive':
+#         del users[user]
  
+print(len(arr) > 0)
+val = 0
+
+while len(arr) < 7:
+    val += 1
+    arr.append(val)
+
+
+# print(len(arr))
+arr.extend([3,12,67])
+val1 = 0
+
+for i in arr:
+    val1 += i
+    print(val1) 
+    
+val3 = 0 
+for i in range(len(arr)):
+    print('this is normal array :', arr[i])
+    val3 += arr[i]
+    print('this is value after sum : ', val3)
+    
+print(val1 == val3)    
+
+print('this is content of value 3 : ',val3)
+
+print(arr)     
+
+
+
+    
+    
+
+
+
