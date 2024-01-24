7ebd8991caeb14bd3afc9fe59f4a957c3a4a0b7c
mdyousufhossain <yousafhossain3@gmail.com>
2024-01-24 23:18:13 +0600

    adding python response

--- Pyhton_Basic/module/importingchoice.py
+++ Pyhton_Basic/module/importingchoice.py
@@ -1,6 +1,11 @@
 import sys
 
+bal = 2
 
-print('whats your name : ? ' , sys.argv[1])
+if len(sys.argv) < 2:
+    sys.exit('too few argument')
 
+
+for arg in sys.argv[1:-1]:
+    print('hello my name is ' , arg)
 
